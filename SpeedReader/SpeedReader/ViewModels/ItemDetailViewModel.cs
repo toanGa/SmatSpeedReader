bfaa6df4fb19ae60cb49fea319ba8af616f92bf0
Toan Tran <trantoan20122582@gmail.com>
2020-08-18 22:59:52 +0700

    init branch speed reade

---
+++ SpeedReader/SpeedReader/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SpeedReader.Models;
+
+namespace SpeedReader.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
